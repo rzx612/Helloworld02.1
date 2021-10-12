ede26370047f52a1af2e74a919448e304df4bd33
RZX <850631577@qq.com>
2021-10-12 20:35:19 +0800

    first commit

---
+++ src/RZX/Helloword.java
@@ -0,0 +1,11 @@
+package RZX;
+
+public class Helloword {
+    private String name;
+    public void setName( Sstring name){
+        this.name = name ;
+    }
+    public void sayHello( ){
+        system.out.println("He1lo " +name ) ;
+    }
+}
---
+++ src/RZX/main.java
@@ -0,0 +1,8 @@
+package RZX;
+
+public class main {
+    public static void main(string[] args) {
+        Hellowor1d helloworld = new Helloworld( );hellowor1d.setName( "spring" );
+        helloworld.sayHello( ) ;
+    }
+}
